f233a40f4f0ffbb183fead638424855e41d527de
Eli Sadoff <snood1205@gmail.com>
2016-11-16 17:12:11 -0500

    intial

---
+++ lib/symrym/version.rb
@@ -0,0 +1,18 @@
+module Symrym
+  ####################################################################
+  # A note on semantic versioning                                    #
+  # The versioning principles of 'symrym' will be                    #
+  # nearly identical to those followed by ruby and                   #
+  # other similar styles. Thus, a version is of the format           #
+  # VERSION = x.y.z                                                  #
+  # x -> major version                                               #
+  # y -> minor version                                               #
+  # z -> patch/service update                                        #
+  # There is no guarantee of full back compatibility between         #
+  # major versions, but within a major version there is full         #
+  # back compatibility. This means that code written with            #
+  # 0.9 specs, for example, might not run in 1.0, but code written   #
+  # with 0.1 specs will run in 0.9.                                  #
+  ####################################################################
+  VERSION = '0.1.0'
+end
---
+++ symrym.gemspec
@@ -0,0 +1,18 @@
+Gem::Specification.new do |s|
+  s.name        = 'symrym'
+  s.version     = '0.1.0'
+  s.date        = '2016-11-16'
+  s.summary     = 'A symbolic calculator for calculus'
+  s.description = %q|A computer algebra system made primarily
+  for calculus, but also for linear algebra, and other mathematic
+  applications. This is, by far, inferior to products like mathematica
+  or sympy, but is my attempt both to learn how to make a CAS and how
+  to implement the features I like in a CAS. Additionally, this is,
+  to the best of my knowledge, the first fully fledged CAS in Ruby (or
+  at least intends to be by the time it is completed).|
+  s.authors     = ['Eli Sadoff']
+  s.email       = 'snood1205@gmail.com'
+  s.files       = ['lib/symrym.rb']
+  s.homepage    = 'https://github.com/snood1205/symrym'
+  s.license     = 'MIT'
+end
